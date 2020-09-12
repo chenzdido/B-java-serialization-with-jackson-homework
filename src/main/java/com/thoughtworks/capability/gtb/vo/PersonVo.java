4058a2e883135b8a848f319df9594ab5d468cd59
chenzdido <chenzdido@gmail.com>
2020-09-12 23:15:44 +0800

    [zhe] feat:person age serializer.

--- src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,9 +8,11 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonSerialize(using=PersonVoSerializer.class)
 public class PersonVo {
 
   private String id;
+
   private Integer age;
   private String name;
   private String hobby;
---
+++ src/main/java/com/thoughtworks/capability/gtb/vo/PersonVoSerializer.java
@@ -0,0 +1,27 @@
+package com.thoughtworks.capability.gtb.vo;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+
+public class PersonVoSerializer extends StdSerializer<PersonVo> {
+    protected PersonVoSerializer() {
+        super(PersonVo.class);
+    }
+
+    @Override
+    public void serialize(PersonVo personVo, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        if(personVo.getAge()==null){
+            personVo.setAge(0);
+        }
+        gen.writeStartObject();
+        gen.writeStringField("id",personVo.getId());
+        gen.writeNumberField("age",personVo.getAge());
+        gen.writeStringField("name",personVo.getName());
+        gen.writeStringField("hobby",personVo.getHobby());
+        gen.writeEndObject();
+    }
+}
